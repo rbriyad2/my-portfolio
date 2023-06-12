d9617d6527f13ba7280a62d7266e71040d5ed0fd
Md Rannu Ahmed (Riyad) <37493558+rbriyad2@users.noreply.github.com>
2023-06-12 23:53:58 +0600

    CV Uploaded

---
+++ js.js
@@ -0,0 +1,19 @@
+
+
+function compare(a, b) {
+
+    if (a == b) {
+   
+    return true;
+   
+    } else {
+   
+    return false;
+   
+    }
+   
+   }
+   
+   const result = compare(15, "15");
+   
+   console.log(result);
